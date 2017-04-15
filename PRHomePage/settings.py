4137784c2ad3c10462989e97bd7172873fc7bae5
nunoPireza <nunopireza@gmail.com>
2017-04-15 13:39:23 +0100

    aposta app2

--- PRHomePage/settings.py
+++ PRHomePage/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home.apps.HomepageConfig',
+    'aposta.apps.AppConfig',
 ]
 
 MIDDLEWARE = [
--- aposta/views.py
+++ aposta/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
-def apostas(request):
-    return render(request,'aposta/apostas.html')
+
+def aposta(request):
+    return render(request, 'aposta/aposta.html')
