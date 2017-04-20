f1447e1f51203de6b37f0f1e6c56ec6ddbcd8fb6
Ricardofred <ricardofrederico@hotmail.com>
2017-04-20 23:31:35 +0100

    Areas funcionais

--- PRHomePage/settings.py
+++ PRHomePage/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'evvs28a-%3n4+fk2f+j1nwv4uf&f@!@9u40oq4n538*+!7y1^h'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
