136c25787fe45fa20a4bc16b68b9db3d51d81099
nunoPireza <nunopireza@gmail.com>
2017-04-20 23:13:48 +0100

    database atualizada

--- aposta/models.py
+++ aposta/models.py
@@ -4,15 +4,13 @@ from django.utils import timezone
 
 # Create your models here.
 class Conta(models.Model):
-    numConta = models.SmallIntegerField(primary_key=True)
     saldo = models.IntegerField(default=0)
-    nomeUtilizador = models.CharField(max_length=30,default='')
+    nomeUtilizador = models.CharField(max_length=30)
     email = models.EmailField(max_length=70)
     def __str__(self):
-        return self.numConta
+        return self.id
 
 class Concurso(models.Model):
-    nConcurso = models.SmallIntegerField(primary_key=True)
     dataConcurso = models.DateTimeField('dataconcurso')
     b1 = models.CharField(max_length=2)
     b2 = models.CharField(max_length=2)
@@ -22,14 +20,13 @@ class Concurso(models.Model):
     e1 = models.CharField(max_length=2)
     e2 = models.CharField(max_length=2)
     def __str__(self):
-        return self.nConcurso
+        return self.id
 
     def concurso_valido(self):
         return self.dataConcurso >= timezone.now() - datetime.timedelta(days=1)
 
 
 class Aposta(models.Model):
-    nAposta = models.SmallIntegerField(primary_key=True)
     nConcurso = models.ForeignKey(Concurso, on_delete=models.CASCADE)
     nConta = models.ForeignKey(Conta, on_delete=models.CASCADE)
     dataAposta = models.DateTimeField('dataaposta')
@@ -43,4 +40,4 @@ class Aposta(models.Model):
     e2 = models.CharField(max_length=2)
 
     def __str__(self):
-        return self.nAposta
+        return self.id
