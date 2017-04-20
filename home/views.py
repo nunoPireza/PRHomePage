5854f7d579f611b82c7039d3a746fb940321a022
nunoPireza <nunopireza@gmail.com>
2017-04-20 23:55:32 +0100

    database atualizada (home e aposta)

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
-
+from django.conf import settings
+from  django.core.mail import send_mail
 
 def homepage(request):
     return render(request, 'home/homepage.html')
@@ -30,3 +31,10 @@ def gravaAposta(request):
     q=Questao(questao_texto=texto, pub_data= timezone.now())
     q.save()
     return HttpResponseRedirect(reverse('votacao:index'))
+
+def criarUtilizador(request):
+    titulo = 'Email de confirmação  de registo'
+    mensagem = 'Bem vindo ao site de apostas..bla bla bla'
+    emaildestino = 'nmfpa@iscte.pt'
+    send_mail(titulo,mensagem,settings.EMAIL_HOST_USER,emaildestino,fail_silently=True)
+
