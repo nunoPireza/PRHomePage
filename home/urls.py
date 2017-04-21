e697b8d6e68c19a8576bcc53221f329b10469e63
Ricardofred <ricardofrederico@hotmail.com>
2017-04-21 07:38:43 +0100

    ALteração das urls da  home para novas paginas

--- home/urls.py
+++ home/urls.py
@@ -2,7 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.homepage, name='home'),
+    url(r'^$', views.inicio, name='inicio'),
+    url(r'^exitMenor$', views.exitMenor, name='exitMenor'),
+    url(r'^homepage/$', views.homepage, name='homepage'),
     url(r'^novaconta/$', views.novaconta, name='novaconta'),
     url(r'^areacomum/$', views.areacomum, name='areacomum'),
     url(r'^admin/$', views.admin, name='admin'),
