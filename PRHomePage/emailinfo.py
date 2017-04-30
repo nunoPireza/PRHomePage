37cfac90de4e6f84eec605add7b7a70bc95e4383
nunoPireza <nunopireza@gmail.com>
2017-04-30 12:11:28 +0100

    novo email

--- PRHomePage/emailinfo.py
+++ PRHomePage/emailinfo.py
@@ -1,6 +1,6 @@
 #para gmail ou google aps
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'siteapostaspr@gmail.pt'
+EMAIL_HOST_USER = 'siteapostaspr@gmail.com'
 EMAIL_HOST_PASSWORD = 'projectopr.'
 EMAIL_PORT = 587
